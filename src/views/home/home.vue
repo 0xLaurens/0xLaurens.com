812d6b921ac26155191403ae8d2e3954b6194f8b
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-06-29 18:28:29 +0200

    Home: scroll snap

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,13 +1,47 @@
 <template>
-    <div class="hero min-h-screen bg-base-200">
-        <div class="hero-content text-center">
-            <div class="max-w-md">
-                <h1 class="text-5xl font-bold">Hello there</h1>
-                <p class="py-6">
-                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
-                    exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
-                </p>
-                <button class="btn btn-primary">Get Started</button>
+    <div class="snap-y snap-mandatory h-screen overflow-scroll">
+        <div class="snap-start hero min-h-screen bg-base-500">
+            <div class="hero-content">
+                <div class="max-w-md">
+                    <h1>Hi, I'm</h1>
+                    <h1 class="text-primary text-5xl font-bold">
+                        <span class="tooltip" data-tip="Pronounced Lawrence">Laurens</span>
+                        Weterings<span class="text-accent">.</span>
+                    </h1>
+                    <h1 class="text-3xl pb-6">Software Engineer.</h1>
+                    <h1 class="text-xl text-center">
+                        Computer science student passionate about cyber security and new
+                        technologies.
+                    </h1>
+                </div>
+            </div>
+        </div>
+        <div class="snap-start hero min-h-screen bg-yellow-50">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <div>
+                    <h1 class="text-primary text-5xl font-bold">
+                        About<span class="text-accent">.</span>
+                    </h1>
+                    <p class="py-6 text-base-100">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                        exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+        <div class="snap-start hero min-h-screen bg-base-500">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <div>
+                    <h1 class="text-primary text-5xl font-bold">
+                        Skills<span class="text-accent">.</span>
+                    </h1>
+                    <p class="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                        exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
             </div>
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    dark: {
+      primary: "#ff79c6",
+      secondary: "#bd93f9",
+      accent: "#ffb86c",
+      neutral: "#414558",
+      "base-100": "#282a36",
+      "base-content": "#f8f8f2",
+      info: "#8be9fd",
+      success: "#50fa7b",
+      warning: "#f1fa8c",
+      error: "#ff5555",
+    }
   },
   plugins: [require("daisyui")],
 }
