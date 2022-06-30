45bf7972eee79a7bbb0c1491798ae076c4ca5aba
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-06-30 21:59:29 +0200

    Home: Nav and landing page complete

---
+++ src/components/navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="navbar sticky bg-transparent z-50">
+        <div class="flex-1">
+            <a class="btn btn-ghost normal-case text-xl drop-shadow-lg"
+                ><span class="font-bold text-primary">0x</span>Laurens</a
+            >
+        </div>
+        <div class="flex-none">
+            <ul class="menu menu-horizontal p-0">
+                <li><a>about.</a></li>
+                <li><a>portfolio.</a></li>
+                <li><a>skills.</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script></script>
