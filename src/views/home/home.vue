b16e42ff98e054ad7c1e307212a180c40ab3f349
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-06-30 12:09:16 +0200

    Home: icons added to cards

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -34,11 +34,8 @@
             </div>
         </div>
         <div class="snap-start mx-auto grid place-items-center min-h-screen bg-base-100">
-            <div class="flex gap-4 items-center content-center flex-col lg:flex-row">
-                <img
-                    src="../../assets/skills.svg"
-                    class="mx-auto lg:max-w-md 4xl:max-w-lg rounded-lg"
-                />
+            <div class="flex gap-4 items-center content-center flex-col lg:flex-row max-w-3xl">
+                <img src="../../assets/skills.svg" class="mx-auto sm:max-w-sm rounded-lg" />
                 <div>
                     <h1 class="text-primary text-5xl font-bold">
                         Skills<span class="text-accent">.</span>
@@ -49,45 +46,86 @@
                     </p>
                 </div>
             </div>
-            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 pb-10">
-                <div class="card w-72 h-52 sm:w-64 bg-yellow-50 shadow-xl">
+            <div
+                class="snap-center h-screen sm:h-auto md:snap-none grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 py-10"
+            >
+                <div class="card w-72 h-56 sm:w-64 bg-yellow-50 shadow-xl">
                     <div class="card-body">
                         <h2 class="card-title text-primary">Front-end</h2>
                         <div class="card-actions center">
-                            <div class="badge badge-outline text-base-100">Javascript</div>
-                            <div class="badge badge-outline text-base-100">HTML</div>
-                            <div class="badge badge-outline text-base-100">CSS</div>
-                            <div class="badge badge-outline text-base-100">Vue js</div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/js.svg" /> Javascript
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/html.svg" /> HTML
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/css.svg" />CSS
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/vue.svg" />Vue js
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="card w-72 sm:w-64 bg-yellow-50 shadow-xl">
+                <div class="card w-72 h-56 sm:w-64 bg-yellow-50 shadow-xl">
                     <div class="card-body">
                         <h2 class="card-title text-primary">Back-end</h2>
                         <div class="card-actions center">
-                            <div class="badge badge-outline text-base-100">Node.js</div>
-                            <div class="badge badge-outline text-base-100">Java</div>
-                            <div class="badge badge-outline text-base-100">C++</div>
-                            <div class="badge badge-outline text-base-100">C#</div>
-                            <div class="badge badge-outline text-base-100">MySql</div>
-                            <div class="badge badge-outline text-base-100">MSsql</div>
-                            <div class="badge badge-outline text-base-100">Rust</div>
-                            <div class="badge badge-outline text-base-100">Firebase</div>
-                            <div class="badge badge-outline text-base-100">Heroku</div>
-                            <div class="badge badge-outline text-base-100">Express.js</div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/nodejs.svg" />Node.js
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/java.svg" />Java
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/c++.svg" />C++
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/csharp.svg" />C#
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/mysql.svg" />MySql
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/db.png" />MSsql
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/rust.png" />Rust
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img
+                                    class="h-5 w-5"
+                                    src="../../assets/icons/firebase.png"
+                                />Firebase
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/heroku.svg" />Heroku
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/js.svg" />Express.js
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="card w-72 h-52 sm:w-64 bg-yellow-50 shadow-xl">
+                <div class="card w-72 h-56 sm:w-64 bg-yellow-50 shadow-xl">
                     <div class="card-body">
                         <h2 class="card-title text-primary">Professional</h2>
                         <div class="card-actions center">
-                            <div class="badge badge-outline text-base-100">Jira</div>
-                            <div class="badge badge-outline text-base-100">Git</div>
-                            <div class="badge badge-outline text-base-100">Bash</div>
-                            <div class="badge badge-outline text-base-100">Github</div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/jira.svg" />Jira
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/git.svg" />Git
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/bash.svg" />Bash
+                            </div>
+                            <div class="badge badge-outline text-base-100">
+                                <img class="h-5 w-5" src="../../assets/icons/github.svg" />Github
+                            </div>
                         </div>
                     </div>
                 </div>
