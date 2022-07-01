07beead84c14b3b28f4e4899a44aa0b491c57bb8
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-07-01 13:29:06 +0200

    Home: components registered

--- src/main.js
+++ src/main.js
@@ -9,11 +9,22 @@ import "./index.css";
 import footer from "./components/footer.vue"
 import navbar from "./components/navbar.vue"
 
+// home components
+import about from "./components/home/about.vue"
+import landing from "./components/home/landing.vue"
+import portfolio from "./components/home/portfolio.vue"
+import skills from "./components/home/skills.vue"
+
 const app = createApp(App);
 
 app
     .use(router)
     .component("cfooter", footer)
     .component("NavBar", navbar)
+    // home
+    .component("about", about)
+    .component("landing", landing)
+    .component("portfolio", portfolio)
+    .component("skills", skills)
 
 app.mount('#app')
