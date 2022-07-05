60e4d285d12848417f9c19c10b009b02e1e9c445
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-07-05 13:21:35 +0200

    Landing: scaling

--- src/components/home/landing.vue
+++ src/components/home/landing.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="hero min-h-screen bg-base-500 w-full font-custom">
         <div class="hero-content flex-col lg:flex-row">
-            <img src="../../assets/dev.svg" />
+            <img
+                src="../../assets/dev.svg"
+                class="object-contain max-w-xs md:max-w-s lg:max-w-md xl:max-w-lg 2xl:max-w-xl 3xl:max-w-none"
+            />
             <div class="max-w-md text-center md:text-left">
                 <h1 class="font-medium">Hi, I'm</h1>
                 <h1 class="text-5xl font-bold">
