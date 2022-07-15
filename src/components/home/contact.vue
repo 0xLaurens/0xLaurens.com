a68fe94ae73df93f11d9a6b08c89d3b22dccc520
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-07-15 16:41:02 +0200

    Contact: form layout

--- src/components/home/contact.vue
+++ src/components/home/contact.vue
@@ -8,19 +8,63 @@
                 </h1>
             </div>
 
-            <div class="col-span-3 md:col-span-1 row-span-3 bg-base-200 rounded-2xl rounded-r-none">
-                <img class="rounded-xl md:rounded-tl-none h-full" src="../../assets/contact.svg" />
+            <div class="col-span-3 md:col-span-1 row-span-3 bg-base-200 rounded-3xl rounded-r-none">
+                <img class="h-full" src="../../assets/contact.svg" />
             </div>
 
             <div class="col-span-3 md:col-span-2 row-span-3">
                 <div class="card md:rounded-l-none bg-base-200 h-full">
-                    <div class="card-body">
-                        <h2 class="card-title text-black">Lorem ipsum.</h2>
-                        <div class="line w-32" />
-                        <p class="font-light text-black">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                            tempor
-                        </p>
+                    <div class="card-body text-lg font-normal font-custom">
+                        <form>
+                            <div class="grid md:grid-cols-2 md:gap-6">
+                                <div class="relative z-0 mb-6 w-full group">
+                                    <input
+                                        type="text"
+                                        class="block py-2.5 px-0 w-full bg-transparent border-0 border-b-2 border-black appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
+                                        placeholder=" "
+                                        required
+                                    />
+                                    <label
+                                        class="peer-focus:font-medium absolute text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                        >First name</label
+                                    >
+                                </div>
+                                <div class="relative z-0 mb-6 w-full group">
+                                    <input
+                                        class="block py-2.5 px-0 w-full bg-transparent border-0 border-b-2 border-black appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
+                                        placeholder=" "
+                                        required
+                                    />
+                                    <label
+                                        for="floating_last_name"
+                                        class="peer-focus:font-medium absolute text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                        >Last name</label
+                                    >
+                                </div>
+                            </div>
+                            <div class="relative z-0 mb-6 w-full group">
+                                <input
+                                    type="email"
+                                    class="block py-2.5 px-0 w-full bg-transparent border-0 border-b-2 border-black appearance-none focus:outline-none focus:ring-0 focus:border-primary peer"
+                                    placeholder=" "
+                                    required
+                                />
+                                <label
+                                    for="floating_email"
+                                    class="peer-focus:font-medium absolute text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                                    >Email address</label
+                                >
+                            </div>
+                            <div class="py-2.5">
+                                <label for="message" class="block mb-2">Message</label>
+                                <textarea
+                                    class="textarea bg-inherit textarea-bordered border-black focus:border-primary resize-y w-full capitalize font-custom"
+                                    rows="3"
+                                    placeholder="message"
+                                ></textarea>
+                            </div>
+                            <button class="btn btn-primary w-full text-lg">Submit.</button>
+                        </form>
                     </div>
                 </div>
             </div>
