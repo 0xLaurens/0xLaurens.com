62dcae283d9dafa4afe4efffd017ed63c6168efd
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-07-18 21:57:31 +0200

    Navbar: mobile sidebar/navbar added

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,24 +1,42 @@
 <template>
-    <div class="navbar sticky bg-white shadow-lg z-50 px-16 font-custom text-black">
+    <div class="navbar fixed bg-white shadow-lg z-50 px-5 lg:px-16 font-custom text-black">
         <div class="flex-1">
             <a href="#home" class="btn btn-ghost normal-case text-xl font-bold"
                 ><span class="text-primary">0x</span>Laurens</a
             >
         </div>
         <div class="flex-none">
-            <ul class="menu menu-horizontal text-xl font-normal">
-                <li v-for="(link, index) in links" :key="link" class="inline-block">
+            <ul v-if="!mobileView" class="menu menu-horizontal text-xl font-normal">
+                <li v-for="link in links" :key="link" class="inline-block">
                     <a :class="{ active: link == currentSection }" :href="`#${link}`"
                         >{{ link }}.</a
                     >
                 </li>
             </ul>
+            <div v-if="mobileView">
+                <a @click="openMobileNav = !openMobileNav"> <menu-alt1-icon class="h-10 w-10" /></a>
+            </div>
         </div>
     </div>
+    <aside
+        v-show="openMobileNav"
+        class="flex flex-col fixed h-screen w-36 bg-white top-0 left-0 z-[49] shadow-lg"
+    >
+        <label for="my-drawer-5" class="drawer-overlay"></label>
+        <ul class="menu menu-vertical text-xl font-normal pt-32">
+            <li v-for="link in links" :key="link" class="inline-block">
+                <a :class="{ active: link == currentSection }" :href="`#${link}`">{{ link }}.</a>
+            </li>
+        </ul>
+    </aside>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
+import { MenuAlt1Icon } from "@heroicons/vue/solid";
+
+const mobileView = ref(false);
+const openMobileNav = ref(false);
 
 const currentSection = ref("");
 const links = ["home", "portfolio", "about", "skills", "contact"];
@@ -38,6 +56,15 @@ onMounted(() => {
 
     sections.forEach((section) => {
         observer.observe(section);
+    });
+
+    window.addEventListener("resize", () => {
+        if (window.innerWidth <= 900) {
+            mobileView.value = true;
+        } else {
+            mobileView.value = false;
+            openMobileNav.value = false;
+        }
     });
 });
 </script>
