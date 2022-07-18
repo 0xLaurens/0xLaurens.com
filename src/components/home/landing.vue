2ae8fb1cbbd6a48062d28f13a11fc75e4dc26d1d
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-07-18 14:39:27 +0200

    Navbar: intersection observer

--- src/components/home/landing.vue
+++ src/components/home/landing.vue
@@ -1,34 +1,36 @@
 <template>
-    <div class="hero min-h-screen bg-base-500 w-full font-custom py-20">
-        <div class="hero-content flex-col lg:flex-row">
-            <img
-                src="../../assets/dev.svg"
-                class="object-contain max-w-xs md:max-w-s lg:max-w-md xl:max-w-lg 2xl:max-w-xl 3xl:max-w-none"
-            />
-            <div class="max-w-md text-center md:text-left">
-                <h1 class="font-medium">Hi, I'm</h1>
-                <h1 class="text-5xl font-bold">
-                    <span class="tooltip" data-tip="Pronounced Lawrence">Laurens</span>
-                    Weterings.
-                </h1>
-                <h1 class="text-3xl font-light pb-6">Software Engineer.</h1>
-                <div class="flex justify-between">
-                    <button
-                        class="btn btn-outline btn-primary w-[45%] font-extrabold text-base rounded-xl"
-                        href="#about"
-                    >
-                        <user-icon class="h-5 w-5 mr-2" /> about me
-                    </button>
-                    <button
-                        class="btn btn-primary justify-self-end w-[50%] font-extrabold text-base rounded-xl"
-                        href="#portfolio"
-                    >
-                        <eye-icon class="h-5 w-5 mr-2" /> portfolio
-                    </button>
+    <section id="home">
+        <div class="hero min-h-screen bg-base-500 w-full font-custom py-20">
+            <div class="hero-content flex-col lg:flex-row">
+                <img
+                    src="../../assets/dev.svg"
+                    class="object-contain max-w-xs md:max-w-s lg:max-w-md xl:max-w-lg 2xl:max-w-xl 3xl:max-w-none"
+                />
+                <div class="max-w-md text-center md:text-left">
+                    <h1 class="font-medium">Hi, I'm</h1>
+                    <h1 class="text-5xl font-bold">
+                        <span class="tooltip" data-tip="Pronounced Lawrence">Laurens</span>
+                        Weterings.
+                    </h1>
+                    <h1 class="text-3xl font-light pb-6">Software Engineer.</h1>
+                    <div class="flex justify-between">
+                        <button
+                            class="btn btn-outline btn-primary w-[45%] font-extrabold text-base rounded-xl"
+                            href="#about"
+                        >
+                            <user-icon class="h-5 w-5 mr-2" /> about me
+                        </button>
+                        <button
+                            class="btn btn-primary justify-self-end w-[50%] font-extrabold text-base rounded-xl"
+                            href="#portfolio"
+                        >
+                            <eye-icon class="h-5 w-5 mr-2" /> portfolio
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </template>
 
 <script setup>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="navbar sticky bg-white/60 z-50 px-16 font-custom text-black">
+    <div class="navbar sticky bg-white shadow-lg z-50 px-16 font-custom text-black">
         <div class="flex-1">
             <a class="btn btn-ghost normal-case text-xl font-bold"
                 ><span class="text-primary">0x</span>Laurens</a
@@ -17,4 +17,22 @@
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+
+const currentSection = ref("");
+onMounted(() => {
+    const sections = document.querySelectorAll("section");
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.intersectionRatio > 0) {
+                console.log(entry.target.getAttribute("id"));
+            }
+        });
+    });
+
+    sections.forEach((section) => {
+        observer.observe(section);
+    });
+});
+</script>
