d3cd60211eaaeaabcb768ecc1ef7ccba778b64a8
lauwIV <64641417+lauwIV@users.noreply.github.com>
2022-07-24 15:25:22 +0200

    Portfolio: scaling better on large screen

--- src/components/home/portfolio.vue
+++ src/components/home/portfolio.vue
@@ -10,15 +10,14 @@
             :class="{ 'flex-row-reverse': index % 2 === 0 }"
         >
             <img
-                class="rounded-xl max-w-2xl"
+                class="rounded-xl w-3/5"
                 src="https://www.datocms-assets.com/38195/1621499794-feedhive-io-macbook-pro-3.png"
             />
-            <div class="self-center px-10 max-w-md">
-                <h2 class="text-3xl underline underline-offset-4 font-bold">Project name.</h2>
+            <div :class="{ 'pl-0': index % 2 === 0 }" class="self-center px-10 w-2/5">
+                <h2 class="text-3xl underline underline-offset-4 font-bold">{{ project.title }}</h2>
                 <span class="text-xs">May 2022</span>
                 <p class="py-2 font-light text-lg 2xl:text-2xl">
-                    Lorem ipsum dolor ismet achmed, doloris ipsumis Lorem ipsum dolor ismet achmed,
-                    doloris ipsumis Lorem ipsum dolor ismet achmed, doloris ipsumis
+                    {{ project.short }}
                 </p>
                 <button
                     class="btn btn-primary font-extrabold text-xl rounded-xl hover:bg-primary-focus w-full my-5"
