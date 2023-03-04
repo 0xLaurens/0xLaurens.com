6c560239c4207c105a9f336e4543973ce4310dc4
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-03-04 09:46:28 +0100

    Tailwindcss -> init config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
+<div class="profile">
+    <div class="profile-picture">
+        <img src="logo.jpg" alt="profile" />
+    </div>
+    <div>
+        <div class="name">
+            <h3>Laurens Weterings.</h3>
+        </div>
+        <div class="titles">
+            <h5>Software Developer | Computer Scientist</h5>
+        </div>
+    </div>
+    <div class="icons" />
+</div>
+<div class="about">
+    <p>
+        Hello! I'm a Software Engineering student at Avans Hogeschool. I'm currently tinkering a lot
+        in Linux, learning more about C and Rust. I enjoy tinkering with new technologies and
+        learning more about how the tech we use works. Take a look at some of my projects
+    </p>
+</div>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
