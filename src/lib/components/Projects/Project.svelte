a6a23050e18db8bb25460e55325789f296bdbb3c
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-03-04 20:25:47 +0100

    projects (project) -> project component

---
+++ src/lib/components/Projects/Project.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import type { Project } from "$lib/models/project.interface";
+    export let project: Project = {
+        title: "default title",
+        description: "default description",
+        tags: ["default 0", "default 1", "default 2"],
+        github: "https://github.com/0xLaurens",
+    };
+</script>
+
+<div class="card bg-base-200 shadow-xl">
+    <div class="card-body">
+        <h2 class="card-title">{project.title}</h2>
+        <p>{project.description}</p>
+        <div class="card-actions">
+            {#each project.tags as tag}
+                <div class="badge badge-secondary">{tag}</div>
+            {/each}
+        </div>
+        <div class="card-actions pt-6 justify-between">
+            <a target="_blank" rel="noreferrer" class="btn btn-primary" href={project.github}
+                >Repository <i class="pl-2 bi bi-github" />
+            </a>
+            {#if project.website}
+                <a target="_blank" rel="noreferrer" class="btn btn-primary" href={project.website}
+                    >Website<i class="bi bi-box-arrow-up-right pl-2" />
+                </a>
+            {/if}
+        </div>
+    </div>
+</div>
---
+++ src/routes/Projects/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Project from "$lib/components/Projects/Project.svelte";
+</script>
+
+<div class="title">
+    <h3 class="text-3xl  font-bold">Projects</h3>
+</div>
+
+<div class="projects pt-3">
+    <Project />
+</div>
