6b7500ae08aa6a05fb3e4f11e9956520d65f7be3
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-03-04 16:39:11 +0100

    lib (Navbar) -> same width as the rest of layout

--- src/lib/components/Navbar/Navbar.svelte
+++ src/lib/components/Navbar/Navbar.svelte
@@ -8,7 +8,9 @@
 </script>
 
 <div class="fixed w-full">
-    <div class="container flex flex-wrap items-center justify-between mx-auto py-3">
+    <div
+        class="container flex flex-wrap items-center justify-between mx-auto py-3 px-3 md:px-0 sm:w-full md:w-4/5 lg:w-3/5 "
+    >
         <div class="links space-x-10 font-semibold text-xl">
             <a class="link link-hover" href="/">Home</a>
             <a class="link link-hover" href="/">Projects</a>
