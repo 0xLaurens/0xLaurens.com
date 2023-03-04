28a85d2c07a3af48f574781bcbe212d79bfe2ba1
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-03-04 10:07:38 +0100

    App (tailwind) -> styling through tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,6 +19,4 @@
         learning more about how the tech we use works. Take a look at some of my projects
     </p>
 </div>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button class="btn btn-primary">test</button>
