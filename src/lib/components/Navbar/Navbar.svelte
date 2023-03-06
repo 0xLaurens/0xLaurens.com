1816046279ef59605a1065d49ed71bf3f4b8abbc
lauwIV <64641417+lauwIV@users.noreply.github.com>
2023-03-06 20:50:33 +0100

    background

--- src/lib/components/Navbar/Navbar.svelte
+++ src/lib/components/Navbar/Navbar.svelte
@@ -7,7 +7,7 @@
     });
 </script>
 
-<div class="fixed w-full">
+<div class="fixed w-full z-50 bg-base-100">
     <div
         class="container flex flex-wrap items-center justify-between mx-auto py-3 px-3 md:px-0 sm:w-full md:w-4/5 lg:w-3/5 "
     >
