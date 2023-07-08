10ed428234ea75e7955368d0b16647fb8e224778
0xLaurens <lauwiv@gmail.com>
2023-07-08 20:57:50 +0200

    Security (vulnerability) -> npm audit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js,svelte,ts}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [require("@tailwindcss/typography"), require("daisyui")],
-    daisyui: {
-        theme: ["dracula"],
-    },
-};
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		require('path').join(require.resolve(
+			'@skeletonlabs/skeleton'),
+			'../**/*.{html,js,svelte,ts}'
+		)
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		...require('@skeletonlabs/skeleton/tailwind/skeleton.cjs')()
+	]
+}
+
