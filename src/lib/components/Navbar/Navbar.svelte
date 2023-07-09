24ec843dfaf7955a41de7b2873d946301ef7d439
0xLaurens <lauwiv@gmail.com>
2023-07-09 11:03:59 +0200

    Navbar (hamburger) -> hamburger icon on small devices

--- src/lib/components/Navbar/Navbar.svelte
+++ src/lib/components/Navbar/Navbar.svelte
@@ -1,15 +1,21 @@
 <script lang="ts">
   import { LightSwitch } from "@skeletonlabs/skeleton";
+  import HamburgerIcon from "$lib/components/Icons/HamburgerIcon.svelte";
 </script>
 
 <div
-  class="container flex flex-wrap items-center justify-between mx-auto py-3 px-3 md:px-0 sm:w-full md:w-4/5 lg:w-3/5"
+  class="container flex flex-wrap items-center justify-between mx-auto py-3 px-3 sm:w-full md:w-4/5 lg:w-3/5"
 >
-  <div class="links space-x-10 font-semibold text-xl hidden sm:flex">
-    <a class="link link-hover" href="/">Home</a>
-    <a class="link link-hover" href="/Projects">Projects</a>
+  <div class="sm:hidden">
+    <button class="btn btn-icon">
+      <HamburgerIcon />
+    </button>
   </div>
-  <div class="theme-switch self-end">
+  <div class="links space-x-10 font-semibold text-xl hidden sm:flex">
+    <a class="link hover:underline" href="/">Home</a>
+    <a class="link hover:underline" href="/Projects">Projects</a>
+  </div>
+  <div class="theme-switch">
     <LightSwitch />
   </div>
 </div>
