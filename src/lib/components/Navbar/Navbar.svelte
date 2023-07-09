592a0ade91d57bff7d161da879c01adea08f142e
0xLaurens <lauwiv@gmail.com>
2023-07-09 11:25:07 +0200

    Navbar (drawer) -> drawer for phones

--- src/lib/components/Navbar/Navbar.svelte
+++ src/lib/components/Navbar/Navbar.svelte
@@ -1,19 +1,40 @@
 <script lang="ts">
   import { LightSwitch } from "@skeletonlabs/skeleton";
   import HamburgerIcon from "$lib/components/Icons/HamburgerIcon.svelte";
+  import CloseIcon from "$lib/components/Icons/CloseIcon.svelte";
+  import { Drawer, drawerStore } from "@skeletonlabs/skeleton";
+  import type { DrawerSettings } from "@skeletonlabs/skeleton";
+  import NavigationLinks from "$lib/components/Navbar/NavigationLinks.svelte";
+
+  const drawerSettings: DrawerSettings = {
+    position: "left",
+    blur: true,
+  };
 </script>
+
+<Drawer {drawerSettings}>
+  <div class="flex flex-col p-6 font-semibold text-xl">
+    <div class="self-end">
+      <button class="btn btn-icon" on:click={drawerStore.close}
+        ><CloseIcon />
+      </button>
+    </div>
+    <div class="flex flex-col h-full justify-between space-y-3">
+      <NavigationLinks />
+    </div>
+  </div>
+</Drawer>
 
 <div
   class="container flex flex-wrap items-center justify-between mx-auto py-3 px-3 sm:w-full md:w-4/5 lg:w-3/5"
 >
   <div class="sm:hidden">
-    <button class="btn btn-icon">
+    <button class="btn btn-icon" on:click={drawerStore.open}>
       <HamburgerIcon />
     </button>
   </div>
   <div class="links space-x-10 font-semibold text-xl hidden sm:flex">
-    <a class="link hover:underline" href="/">Home</a>
-    <a class="link hover:underline" href="/Projects">Projects</a>
+    <NavigationLinks />
   </div>
   <div class="theme-switch">
     <LightSwitch />
---
+++ src/lib/components/Navbar/NavigationLinks.svelte
@@ -0,0 +1,2 @@
+ <a class="link hover:underline" href="/">Home</a>
+ <a class="link hover:underline" href="/Projects">Projects</a>
