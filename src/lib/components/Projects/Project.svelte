ab8d1adb2b5c42766a65cb525c6edd43ece98d0d
0xLaurens <lauwiv@gmail.com>
2023-07-10 19:22:12 +0200

    Light (mode) -> slate as backdrop

--- src/lib/components/Projects/Project.svelte
+++ src/lib/components/Projects/Project.svelte
@@ -9,31 +9,33 @@
   };
 </script>
 
-<div class="card shadow-xl mb-5 p-4 text-center md:text-start">
-    <h2 class="text-2xl font-bold">{project.title}</h2>
-    <p class="mb-5">{project.description}</p>
-    <div class="card-actions mb-5">
-      {#each project.tags as tag}
-        <div 
-        class="badge variant-filled-secondary mr-2">{tag}</div>
-      {/each}
-    </div>
-    <div class="flex justify-center md:justify-end">
+<div
+  class:!bg-slate-100={$modeCurrent}
+  class="card shadow-xl mb-5 p-4 text-center md:text-start"
+>
+  <h2 class="text-2xl font-bold">{project.title}</h2>
+  <p class="mb-5">{project.description}</p>
+  <div class="card-actions mb-5">
+    {#each project.tags as tag}
+      <div class="badge variant-filled-secondary mr-2">{tag}</div>
+    {/each}
+  </div>
+  <div class="flex justify-center md:justify-end">
+    <a
+      target="_blank"
+      rel="noreferrer"
+      class="btn btn-sm variant-filled-primary"
+      href={project.github}
+      >Repository <i class="pl-2 bi bi-github" />
+    </a>
+    {#if project.website}
       <a
         target="_blank"
         rel="noreferrer"
-        class="btn btn-sm variant-filled-primary"
-        href={project.github}
-        >Repository <i class="pl-2 bi bi-github" />
+        class="btn btn-sm variant-ghost-primary ml-2"
+        href={project.website}
+        >Website<i class="bi bi-box-arrow-up-right pl-2" />
       </a>
-      {#if project.website}
-        <a
-          target="_blank"
-          rel="noreferrer"
-          class="btn btn-sm variant-ghost-primary ml-2"
-          href={project.website}
-          >Website<i class="bi bi-box-arrow-up-right pl-2" />
-        </a>
-      {/if}
-    </div>
+    {/if}
+  </div>
 </div>
